ab3a6fcf35e14d1c4dc3518bb563958c1522bd65
Peter Chahid <peterdaaboul@outlook.com>
2023-07-25 11:35:37 +0300

    second commit

--- src/Panel.js
+++ src/Panel.js
@@ -1,25 +1,68 @@
 import "./Panel.css";
+import React from "react";
 export default function Panel() {
+  const [obj, setobj] = React.useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    checkPoint: false,
+  });
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    const inputValue = type === "checkbox" ? checked : value;
+
+    setobj({
+      ...obj,
+      [name]: inputValue,
+    });
+  }
   function handleSubmit(event) {
     event.preventDefault();
+    if (obj.password === obj.passwordConfirm && obj.checkPoint == true) {
+      console.log("Password matches confirmation. Form submitted! \n");
+      console.log("Thanks for signing up for our newsletter!");
+      // You can perform additional form submission logic here.
+    } else if (obj.password === obj.passwordConfirm) {
+      console.log("Password matches confirmation. Form submitted!");
+    } else {
+      console.log("Password does not match confirmation. Please check again.");
+      // You can show an error message to the user, etc.
+    }
   }
+
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
         <input
           type="email"
+          name="email"
           placeholder="Email address"
           className="form--input"
+          onChange={handleChange}
         />
-        <input type="password" placeholder="Password" className="form--input" />
         <input
           type="password"
+          placeholder="Password"
+          className="form--input"
+          name="password"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="passwordConfirm"
           placeholder="Confirm password"
           className="form--input"
+          onChange={handleChange}
         />
 
         <div className="form--marketing">
-          <input id="okayToEmail" type="checkbox" />
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            name="checkPoint"
+            checked={obj.checkPoint}
+            onChange={handleChange}
+          />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
         <button className="form--submit">Sign up</button>
